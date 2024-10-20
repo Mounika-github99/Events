32239ba00e3298d8344324a88125c97c8f037d38
mochu <mouni@LAPTOP-TSM6EFA2>
2024-10-20 15:16:43 +0530

    first commit

--- vite-project/src/components/Services.jsx
+++ vite-project/src/components/Services.jsx
@@ -1,54 +1,50 @@
 import React from "react";
-
 const Services = () => {
   const services = [
     {
       id: 1,
-      url: "/birthday.jpg",
+      url: "birthday.jpg",
       title: "Birthday Event",
     },
     {
       id: 2,
-      url: "/anniversary.jpg",
+      url: "anniversary.jpg",
       title: "Anniversary Event",
     },
     {
       id: 3,
-      url: "/entertainment event.jpg",
+      url: "entertainment event.jpg",
       title: "Entertainment Event",
     },
     {
       id: 4,
-      url: "/professional event.jpg",
+      url: "professional event.jpg",
       title: "Professional Event",
     },
     {
       id: 5,
-      url: "/party.jpg",
+      url: "party.jpg",
       title: "Party Event",
     },
     {
       id: 6,
-      url: "/wedding.jpg",
+      url: "wedding.jpg",
       title: "Wedding Event",
     },
   ];
+
   return (
-    <>
-      <div className="services container">
-        <h2>OUR SERVICES</h2>
-        <div className="banner">
-          {services.map((element) => {
-            return (
-              <div className="item" key={element.id}>
-                <h3>{element.title}</h3>
-                <img src={element.url} alt={element.title} />
-              </div>
-            );
-          })}
-        </div>
+    <div className="services container">
+      <h2>OUR SERVICES</h2>
+      <div className="banner">
+        {services.map((service) => (
+          <div className="item" key={service.id}>
+            <h3>{service.title}</h3>
+            <img src={service.url} alt={service.title} />
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
